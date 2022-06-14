b874e3eec9d18dab6a1be5994a49af32d0658f35
Edward <edwardevbert@gmail.com>
2022-06-14 16:59:27 +0700

    update

--- resources/views/dashboard/product/index.blade.php
+++ resources/views/dashboard/product/index.blade.php
@@ -42,7 +42,7 @@
                                 <th>Sub Category</th>
                                 <th class="text-center">Actions</th>
                             </tr>
-                        </thead>
+                        </thead>~
                         <tbody>
                             @foreach ($products as $product)
                                 <tr>
