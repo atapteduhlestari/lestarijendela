a1e92c98b6b81b72afa483e5531763fd814bda1b
Edward <edwardevbert@gmail.com>
2022-10-21 11:55:41 +0700

    update

--- resources/views/layouts/visitor/components/quotes.blade.php
+++ resources/views/layouts/visitor/components/quotes.blade.php
@@ -3,7 +3,7 @@
         <div class="col-12 txt-center m-b-25">
             <em>
                 <h3 class="ltext-106 cl0">
-                    &ldquo; Adventure is just outside your <span class="cl1">window &rdquo;</span>
+                    <span class="cl1">&ldquo;</span> Taking The Outside In <span class="cl1">&rdquo;</span>
                 </h3>
             </em>
         </div>
